dcabc217197b41e0ddff7aa46a7ff6e3a2d3b957
AlessioDPT <alessio.recos@gmail.com>
2021-06-21 15:51:42 +0200

    ES- 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 // Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue ({
+
+    el : '#root',
+
+    data : {
+        messaggio : "Questo è un messaggio qualsiasi di un esempio qualsiasi di un esercizio qualsiasi.",
+
+        img : "https://www.sinonimi-contrari.it/includes/images/sharers/sinonimi-di-qualsiasi.png"
+    }
+
+});
